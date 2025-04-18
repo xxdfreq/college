46dfcb3b6636dee16fd808051c1f80eb81618083
Andrey <agavrilin1988@gmail.com>
2025-04-18 16:59:24 +0300

    Update Module 2 to accept a value

--- src/module2/module2.py
+++ src/module2/module2.py
@@ -9,7 +9,7 @@ if choice == "1":
     fahrenheit = (celsius * 9/5) + 32
     print(f"{celsius}°C = {fahrenheit:.1f}°F")
 elif choice == "2":
-    fahrenheit = float(input("Введите градусы Фаренгейта: "))
+    fahrenheit = float(input("Введите градусы Фаренгейта): "))
     celsius = (fahrenheit - 32) * 5/9
     print(f"{fahrenheit}°F = {celsius:.1f}°C")
 else:
